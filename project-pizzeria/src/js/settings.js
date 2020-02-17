ae9a4bc612738c624a0a8475339ed533ac48e5f9
Łukasz Potoczniak <lukaszpotoczniak@gmail.com>
2020-02-17 18:10:13 +0100

    add 10.2 point ,everythink working well

--- project-pizzeria/src/js/settings.js
+++ project-pizzeria/src/js/settings.js
@@ -41,7 +41,6 @@ export const select = {
       output: '.output',
     },
   },
-  // CODE ADDED START
   cart: {
     productList: '.cart__order-summary',
     toggleTrigger: '.cart__summary',
@@ -125,5 +124,5 @@ export const settings = {
 export const templates = {
   menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
-  //bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
+  bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
 };
