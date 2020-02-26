37a6fe90b1420b54af4ca9485b513796fd1aead7
Łukasz Potoczniak <lukaszpotoczniak@gmail.com>
2020-02-26 21:58:32 +0100

    working all point from ex 10

--- project-pizzeria/src/js/components/Booking.js
+++ project-pizzeria/src/js/components/Booking.js
@@ -2,6 +2,7 @@ import { select, templates, settings } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
 import utils from '../utils.js';
 import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking{
   constructor(element){
@@ -83,7 +84,8 @@ class Booking{
       thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
     }
     for (let item of eventsRepeat){
-      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
+      if(item.repeat == 'daily')
+        thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
     }
 
     console.log('thisBooking.booked', thisBooking.booked);
@@ -117,12 +119,14 @@ class Booking{
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
     thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
   }
   initWidgets() {
     const thisBooking = this;
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
   }
    
 }
